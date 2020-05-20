a5a99fe904ed2c3b567a5db43807366b5537d825
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-05-20 11:45:07 +1000

    [libcurl.h] Replace `CURLOPT_SSL_CIPHER_LIST` to resolve 'string literal of length 1208 exceeds maximum length 509 that C90 compilers are required to support' error, with standard `CURLOPT_SSLVERSION`; [CMakeLists.txt] Cleanup `add_compile_options` and ignore missing braces (`NULL` on macOS' clang)

--- src/libcurl.h
+++ src/libcurl.h
@@ -216,28 +216,9 @@ int download(const char *url, const char *checksum, const char *target_directory
         curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
     else
         curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
-    /* >=TLS1.2  */
-    curl_easy_setopt(curl, CURLOPT_SSL_CIPHER_LIST, "NULL-SHA256 AES128-SHA256 AES256-SHA256 AES128-GCM-SHA256 "
-                                                    "AES256-GCM-SHA384 DH-RSA-AES128-SHA256 DH-RSA-AES256-SHA256 "
-                                                    "DH-RSA-AES128-GCM-SHA256 DH-RSA-AES256-GCM-SHA384 "
-                                                    "DH-DSS-AES128-SHA256 DH-DSS-AES256-SHA256 DH-DSS-AES128-GCM-SHA256 "
-                                                    "DH-DSS-AES256-GCM-SHA384 DHE-RSA-AES128-SHA256 DHE-RSA-AES256-SHA256 "
-                                                    "DHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 "
-                                                    "DHE-DSS-AES128-SHA256 DHE-DSS-AES256-SHA256 DHE-DSS-AES128-GCM-SHA256 "
-                                                    "DHE-DSS-AES256-GCM-SHA384 ECDHE-RSA-AES128-SHA256 "
-                                                    "ECDHE-RSA-AES256-SHA384 ECDHE-RSA-AES128-GCM-SHA256 "
-                                                    "ECDHE-RSA-AES256-GCM-SHA384 ECDHE-ECDSA-AES128-SHA256 "
-                                                    "ECDHE-ECDSA-AES256-SHA384 ECDHE-ECDSA-AES128-GCM-SHA256 "
-                                                    "ECDHE-ECDSA-AES256-GCM-SHA384 ADH-AES128-SHA256 ADH-AES256-SHA256 "
-                                                    "ADH-AES128-GCM-SHA256 ADH-AES256-GCM-SHA384 AES128-CCM AES256-CCM "
-                                                    "DHE-RSA-AES128-CCM DHE-RSA-AES256-CCM AES128-CCM8 AES256-CCM8 "
-                                                    "DHE-RSA-AES128-CCM8 DHE-RSA-AES256-CCM8 ECDHE-ECDSA-AES128-CCM "
-                                                    "ECDHE-ECDSA-AES256-CCM ECDHE-ECDSA-AES128-CCM8 ECDHE-ECDSA-AES256-CCM8 "
-                                                    "ECDHE-ECDSA-CAMELLIA128-SHA256 ECDHE-ECDSA-CAMELLIA256-SHA384 "
-                                                    "ECDHE-RSA-CAMELLIA128-SHA256 ECDHE-RSA-CAMELLIA256-SHA384 "
-                                                    "TLS_AES_256_GCM_SHA384 TLS_CHACHA20_POLY1305_SHA256 "
-                                                    "TLS_AES_128_GCM_SHA256 TLS_AES_128_CCM_8_SHA256 "
-                                                    "TLS_AES_128_CCM_SHA256");
+
+    /* ask libcurl to use TLS version 1.2 or later */
+    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 
     curl_easy_setopt(curl, CURLOPT_VERBOSE, verbosity);
 
