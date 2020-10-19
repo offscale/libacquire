662f5664bae96c82b844433527aaf768acea67ca
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-10-19 16:05:17 +1100

    [tests/src/test_fileutils.h] Use different integer types dependending on compiler being MSVC or not (non MSVC are C89 compatible…)

--- tests/src/test_fileutils.h
+++ tests/src/test_fileutils.h
@@ -3,14 +3,22 @@
 #include <fileutils.h>
 #include <config_for_tests.h>
 
+#ifdef _MSC_BUILD
+#define NUM_FORMAT "%zu"
+typedef unsigned size_t num_type;
+#else
+#define NUM_FORMAT "%lu"
+typedef unsigned long num_type;
+#endif
+
 TEST x_is_directory_should_be_true(void) {
     const bool x = is_directory(CMAKE_CURRENT_SOURCE_DIR);
-            ASSERT_EQ_FMT((size_t) true, x, "%zu");
+            ASSERT_EQ_FMT((num_type) true, x, NUM_FORMAT);
             PASS();
 }
 
 TEST x_is_directory_should_be_false(void) {
-    const bool x  = is_directory(BAD_DIR);
+    const bool x = is_directory(BAD_DIR);
     const bool y = is_directory(CMAKE_CURRENT_LIST_FILE);
     ASSERT_FALSE(x);
     ASSERT_FALSE(y);
@@ -19,38 +27,38 @@ TEST x_is_directory_should_be_false(void) {
 
 TEST x_is_file_should_be_true(void) {
     const bool x = is_file(CMAKE_CURRENT_LIST_FILE);
-            ASSERT_EQ_FMT((size_t) true, x, "%zu");
+            ASSERT_EQ_FMT((num_type) true, x, "%zu");
             PASS();
 }
 
 TEST x_is_file_should_be_false(void) {
-    const bool x  = is_file(CMAKE_CURRENT_SOURCE_DIR);
-    const bool y  = is_directory(BAD_DIR);
-    ASSERT_FALSE(x);
-    ASSERT_FALSE(y);
-    PASS();
+    const bool x = is_file(CMAKE_CURRENT_SOURCE_DIR);
+    const bool y = is_directory(BAD_DIR);
+            ASSERT_FALSE(x);
+            ASSERT_FALSE(y);
+            PASS();
 }
 
 TEST x_exists_should_be_true(void) {
     const bool x = exists(CMAKE_CURRENT_SOURCE_DIR);
     const bool y = exists(CMAKE_CURRENT_LIST_FILE);
-            ASSERT_EQ_FMT((size_t) true, x, "%zu");
-            ASSERT_EQ_FMT((size_t) true, y, "%zu");
+            ASSERT_EQ_FMT((num_type) true, x, NUM_FORMAT);
+            ASSERT_EQ_FMT((num_type) true, y, NUM_FORMAT);
             PASS();
 }
 
 TEST x_exists_should_be_false(void) {
-    const bool x  = exists(BAD_DIR);
-    ASSERT_FALSE(x);
-    PASS();
+    const bool x = exists(BAD_DIR);
+            ASSERT_FALSE(x);
+            PASS();
 }
 
 /* Suites can group multiple tests with common setup. */
-SUITE(fileutils_suite) {
-    RUN_TEST(x_is_directory_should_be_true);
-    RUN_TEST(x_is_directory_should_be_false);
-    RUN_TEST(x_is_file_should_be_true);
-    RUN_TEST(x_is_file_should_be_false);
-    RUN_TEST(x_exists_should_be_true);
-    RUN_TEST(x_exists_should_be_false);
+SUITE (fileutils_suite) {
+            RUN_TEST(x_is_directory_should_be_true);
+            RUN_TEST(x_is_directory_should_be_false);
+            RUN_TEST(x_is_file_should_be_true);
+            RUN_TEST(x_is_file_should_be_false);
+            RUN_TEST(x_exists_should_be_true);
+            RUN_TEST(x_exists_should_be_false);
 }
