12d742c391043cfa253594d0754eece57f9a1f14
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-10-29 16:08:00 -0400

    [libacquire/freebsd_libfetch] Generate ftperr.h and httperr.h from input template as per original shell script (but rewritten in CMake)

--- libacquire/freebsd_libfetch/ftperr.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBACQUIRE_FTPERR_H
-#define LIBACQUIRE_FTPERR_H
-
-static struct fetcherr ftp_errlist[] = {
-        /* TODO: Generate from ftp.errors */
-};
-
-#endif /* LIBACQUIRE_FTPERR_H */
--- libacquire/freebsd_libfetch/httperr.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBACQUIRE_HTTPERR_H
-#define LIBACQUIRE_HTTPERR_H
-
-static struct fetcherr http_errlist[] = {
-        /* TODO: Generate from http.errors */
-};
-
-#endif /* LIBACQUIRE_HTTPERR_H */
