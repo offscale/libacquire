138df34430a63c412b8167c0a47b6268dd183e92
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-11-22 22:23:38 -0500

    [.github/workflows/github-actions.yml] Debugging Windows

--- libacquire/acquire_zlib.h
+++ libacquire/acquire_zlib.h
@@ -14,83 +14,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include "zlib.h"
 
-#include <minizip/unzip.h>
-#include <minizip/zip.h>
-
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-#include <minizip/iowin32.h>
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
 #endif
 
-#define BUF_SIZE 8192
-#define MAX_NAMELEN 256
+#define CHUNK 16384
 
-enum zipper_result_t {
-    ZIPPER_RESULT_ERROR = 0,
-    ZIPPER_RESULT_SUCCESS,
-    ZIPPER_RESULT_SUCCESS_EOF
-};
-
-typedef void (*zipper_read_cb_t)(const unsigned char *buf, size_t size, void *thunk);
-
-extern bool zipper_add_file(zipFile zfile, const char *filename);
-extern bool zipper_add_buf(zipFile zfile, const char *zfilename, const unsigned char *buf, size_t buflen);
-extern bool zipper_add_dir(zipFile zfile, const char *dirname);
-
-extern enum zipper_result_t zipper_read(unzFile zfile, zipper_read_cb_t cb, void *thunk);
-extern enum zipper_result_t zipper_read_buf(unzFile zfile, unsigned char **buf, size_t *buflen);
-
-extern bool zipper_skip_file(unzFile zfile);
-extern char *zipper_filename(unzFile zfile, bool *isutf8);
-extern bool zipper_isdir(unzFile zfile);
-extern uint64_t zipper_filesize(unzFile zfile);
-
-
-zipper_result_t zipper_read(unzFile zfile, zipper_read_cb_t cb, void *thunk)
+/* Decompress from file source to file dest until stream ends or EOF.
+   inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_DATA_ERROR if the deflate data is
+   invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
+   the version of the library linked do not match, or Z_ERRNO if there
+   is an error reading or writing the files. */
+int inf(FILE *source, FILE *dest)
 {
-    unsigned char tbuf[BUF_SIZE];
-    int           red;
-    int           ret;
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
 
-    if (zfile == NULL || cb == NULL)
-        return ZIPPER_RESULT_ERROR;
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK)
+        return ret;
 
-    ret = unzOpenCurrentFile(zfile);
-    if (ret != UNZ_OK)
-        return ZIPPER_RESULT_ERROR;
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
 
-    while ((red = unzReadCurrentFile(zfile, tbuf, sizeof(tbuf))) > 0) {
-        cb(tbuf, red, thunk);
-    }
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
 
-    if (red < 0) {
-        unzCloseCurrentFile(zfile);
-        return ZIPPER_RESULT_ERROR;
-    }
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
 
-    unzCloseCurrentFile(zfile);
-    if (unzGoToNextFile(zfile) != UNZ_OK)
-        return ZIPPER_RESULT_SUCCESS_EOF;
-    return ZIPPER_RESULT_SUCCESS;
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 }
 
-static void zipper_read_buf_cb(const unsigned char *buf, size_t buflen, void *thunk)
+/* report a zlib or i/o error */
+void zerr(int ret)
 {
-    str_builder_t *sb = thunk;
-    str_builder_add_str(sb, (const char *)buf, buflen);
+    fputs("zpipe: ", stderr);
+    switch (ret) {
+    case Z_ERRNO:
+        if (ferror(stdin))
+            fputs("error reading stdin\n", stderr);
+        if (ferror(stdout))
+            fputs("error writing stdout\n", stderr);
+        break;
+    case Z_STREAM_ERROR:
+        fputs("invalid compression level\n", stderr);
+        break;
+    case Z_DATA_ERROR:
+        fputs("invalid or incomplete deflate data\n", stderr);
+        break;
+    case Z_MEM_ERROR:
+        fputs("out of memory\n", stderr);
+        break;
+    case Z_VERSION_ERROR:
+        fputs("zlib version mismatch!\n", stderr);
+    }
 }
 
-zipper_result_t zipper_read_buf(unzFile zfile, unsigned char **buf, size_t *buflen)
-{
-    str_builder_t   *sb;
-    zipper_result_t  ret;
-
-    sb = str_builder_create();
-    ret = zipper_read(zfile, zipper_read_buf_cb, sb);
-    if (ret != ZIPPER_RESULT_ERROR)
-        *buf = (unsigned char *)str_builder_dump(sb, buflen);
-    str_builder_destroy(sb);
-    return ret;
+int extract_archive(enum Archive archive, const char *archive_filepath, const char *output_folder) {
+    return EXIT_FAILURE;
 }
 
 #endif /* !defined(LIBACQUIRE_ACQUIRE_ZLIB_H) && defined(USE_ZLIB) && defined(LIBACQUIRE_IMPLEMENTATION) */
