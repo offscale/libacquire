3d9220cba9c405a65109436cd3843e463cf142a7
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-11 00:20:41 -0500

    [libacquire/libacquire/acquire_common_defs.h] Set arch for MSVC; [libacquire/libacquire/{"",tests/test_libacquire}/CMakeLists.txt] `set_checksum_lib`

--- libacquire/acquire_common_defs.h
+++ libacquire/acquire_common_defs.h
@@ -82,4 +82,12 @@
 #define NAME_MAX 4096
 #endif /* ! NAME_MAX */
 
+#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
+#define _AMD64_
+#elif defined(i386) || defined(__i386) || defined(__i386__) || defined(__i386__) || defined(_M_IX86)
+#define _X86_
+#elif defined(__arm__) || defined(_M_ARM) || defined(_M_ARMT)
+#define _ARM_
+#endif
+
 #endif /* ! LIBACQUIRE_ACQUIRE_COMMON_DEFS_H */
